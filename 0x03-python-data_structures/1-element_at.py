6e85b1aaca6db79e9af2dc68a1f278725caa6509
Pelumi <odunoyepelumi23@gmail.com>
2022-08-27 17:43:48 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def element_at_my_list, idx):
-    if idx < o and idx  > (len(my_list):
+    if idx < o and idx  > (len(my_list - 1):
             return (none)
     return (my_list[idx])
