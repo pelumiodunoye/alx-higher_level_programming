9057be73cbc5161d3db4f6e923ad9645db8871fe
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-08-29 17:33:25 +0100

    no c/C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    iyanu = ""
+    for a in range(len(my_string)):
+        if my_string[a] != 'c' and my_string[a] != 'C':
+            iyanu = iyanu + my_string[a]
+        return (iyanu)
