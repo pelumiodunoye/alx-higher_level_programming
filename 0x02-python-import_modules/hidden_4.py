b3c015176b1e38a9e271d6cc631cdb7fde0e617f
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-08-30 04:38:38 +0100

    hidden

---
+++ 0x02-python-import_modules/hidden_4.py
@@ -0,0 +1,22 @@
+# uncompyle6 version 3.8.0
+# Python bytecode 3.8.0 (3413)
+# Decompiled from: Python 3.10.2 (main, Mar  1 2022, 12:58:06) [Clang 12.0.8 (https://android.googlesource.com/toolchain/llvm-project c935d99d7
+# Embedded file name: hidden_4.py
+# Compiled at: 2021-07-16 18:16:33
+# Size of source mod 2**32: 146 bytes
+
+
+def __init__():
+    return True
+
+
+def my_secret_santa():
+    return True
+
+
+def print_school():
+    return True
+
+
+def print_hidden():
+    return True
