145fd0cd906c505d8bb32d0dfa57e71f4383d44c
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-08-30 04:41:32 +0100

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+import hidden_4
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-
     names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    n = len(names)
+    for i in range(n):
+        ch = names[i][0]
+        ch1 = names[i][1]
+        if ch != '_' and ch1 != '_':
+            print(names[i])
