23a3f64f3f4f89974831db1c060c7181c0570c20
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-08-31 11:37:56 +0100

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
+
 if __name__ == "__main__":
+    import hidden_4
     names = dir(hidden_4)
-    n = len(names)
-    for i in range(n):
-        ch = names[i][0]
-        ch1 = names[i][1]
-        if ch != '_' and ch1 != '_':
-            print(names[i])
+    for name in names:
+        if name != "__":
+            print(name)
