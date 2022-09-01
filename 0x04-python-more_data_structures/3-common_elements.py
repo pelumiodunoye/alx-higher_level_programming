d911142262d4546ac3c9aaebd84e53632500cfb9
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-09-01 01:40:48 +0100

    common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,8 +3,9 @@
 def common_elements(set_1, set_2):
     a = set_1
     b = set_2
-    
-    if a & b:
-        return (a & b)
-    else:
-        return (None)
+    c = {*()}
+    for a in set_1:
+        for b in set_2:
+            if a == b:
+                c.add(b)
+    return (c)
