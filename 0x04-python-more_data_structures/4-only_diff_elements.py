40cb6fbb953fb0ab99fa82874a45079aba6328b8
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-09-01 02:23:46 +0100

    updated

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,6 +3,11 @@
 def only_diff_elements(set_1, set_2):
     a = set_1
     b = set_2
-    newset = a.union(b)
-    newset.remove ('C')
-    return (newset)
+    d = {*()}
+    d.update(a)
+    d.update(b)
+    for a in set_1:
+        for b in set_2:
+            if a == b:
+                d.remove(b)
+    return (d)
