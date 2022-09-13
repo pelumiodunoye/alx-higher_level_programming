c0a91c41d5cff9af71d8457a324d2aae57b71c40
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-09-14 00:28:18 +0100

    my first square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-
 """Square class definition"""
-
-
 class Square:
-    
     """Represents a square"""
-    
     def __init__(self, size):
-        
         """Initializes a square"""
-        
         self.__size = size
