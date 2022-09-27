0efb9e69a4b135afcbedaf72b8cc1fd621ea9d3f
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-09-27 02:30:02 +0100

    let's try the import way

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
 """
-contains an empty class
+Contains the class BaseGeometry and subclass Rectangle
 """
 
-
-class BaseGeometry:
-    """this is an empty class"""
-    def area(self):
-        """raises an exception when called"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates that value is an integer greater than 0"""
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
