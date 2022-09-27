bdd3aa6b93ab6cb46bafa015e9b6fd263341059f
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-09-27 01:21:38 +0100

    pycode

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,9 @@
 this moule contains the function is_kind_of_class
 """
 
+
 def is_kind_of_class(obj, a_class):
-    """returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class; otherwise False"""
+    """returns True if the object is an instance of,/
+    or if the object is an instance of a class that inherited from/
+    , the specified class; otherwise False"""
     return (isinstance(obj, a_class))
