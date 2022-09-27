d1d8e2bb4677ecfb42709805b9a363842dbf5c60
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-09-27 02:27:05 +0100

    pycode

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,6 +17,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """A representation of a rectangle"""
     def __init__(self, width, height):
