7e66bfafd019584c342d16f99c6e2714b032f415
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-09-28 14:45:17 +0100

    wite to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,8 @@ contains the write_file function
 
 def write_file(filename="", text=""):
     """writes a string to a text file (UTF8) and returns the number of characters written"""
-    
+    with open(filename, 'r', encoding="utf-8") as f:
+        i =0
+        for line in f:
+            i += 1
+        return i 
