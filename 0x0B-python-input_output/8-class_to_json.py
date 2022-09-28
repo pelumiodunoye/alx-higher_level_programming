b5dc05bab34b37f3b2259fcf9b79b6b72775a46a
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-09-28 20:56:28 +0100

    student

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
-We want to return the self init of a class as dictionary
+Contains the "class_to_json" function
 """
 
 
 def class_to_json(obj):
-    return (vars(obj))
+    """returns the dictionary description with simple data structure"""
+    """(list, dictionary, string, integer and boolean)"""
+    """for JSON serialization of an object"""
+    return obj.__dict__
