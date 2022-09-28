e3d8fe36ddbf37d9e9f93119691095a57ab1b859
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-09-28 14:27:08 +0100

    wip

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+contains the write_file function
+"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of characters written"""
+    
