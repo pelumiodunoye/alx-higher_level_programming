2dc0d25b2e1a1032a3c1ca8b4d8b9cba1bfacc11
pelumiodunoye <odunoyepelummi23@gmail.com>
2022-10-02 17:00:22 +0100

    pycoded

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 this contains the rectangle function
@@ -6,9 +5,10 @@ this contains the rectangle function
 
 
 from models.base import Base
+
+
 class Rectangle(Base):
     """contains class rectangle"""
-
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """iniitializes function"""
@@ -18,54 +18,45 @@ class Rectangle(Base):
         self.__x = super().validate(x, "x")
         self.__y = super().validate(y, "y")
 
-
     @property
     def width(self):
         """retrieves the  width of the rectangle"""
         return  (self.__width)
-
 
     @width.setter
     def width(self, value):
         """sets value for width"""
         self.__width = super().validate(value, "width")
 
-
     @property
     def height(self):
         """retrieves the height of the rectangle"""
         return (self.__height)
-
 
     @height.setter
     def height(self, value):
         """sets value for height"""
         self.__height = super().validate(value, "height")
 
-
     @property
     def x(self):
         """retrieves the value of x"""
         return (self.__x)
-
 
     @x.setter
     def x(self, value):
         """sets value for x"""
         self.__x = super().validate(value, "x")
 
-
     @property
     def y(self):
         """retrieves the value of y"""
         return (self.__y)
 
-
     @y.setter
     def y(self, value):
         """sets value for y"""
         self.__y = super().validate(value, "y")
-
 
     def area(self):
         """returns the  area value of the Rectangle instance"""
